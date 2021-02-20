44677f35776e3b315e0737cd40c4621eb7552be2
Gregory-hub <novikov.gri@gmail.com>
2021-02-20 21:41:58 +0700

    Make create_game and join_game views demo versions

---
+++ logic/chessboard.py
@@ -0,0 +1,9 @@
+class ChessBoard:
+    id = None
+
+    def __init__(self, board_id: int = None):
+        pass    # create chessboard in DB and self.id = board_id
+                # or get board from DB and self.id = id of board from DB
+
+    def add_second_player(self):
+        pass
